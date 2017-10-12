09203abb4c759dbd9862d0ba2e4e4cc8fed81314
travbid <tbiddle@redzone.com>
2017-10-13 11:53:46 +1300

    Colours

--- gst.sh
+++ gst.sh
@@ -14,7 +14,16 @@ for d in $dir ; do
     cd "$d"
     git fetch
     echo ""
-    git status
+    res=$(git status)
+    expected="On branch master
+Your branch is up-to-date with 'origin/master'.
+nothing to commit, working tree clean"
+
+    if [[ $res =~ $expected ]]; then
+        echo -e "\e[34m$res\e[0m"
+    else
+        echo -e "\e[91m$res\e[0m"
+    fi
     echo "###############  COMPLETE  ##############"
     echo ""
     echo ""
